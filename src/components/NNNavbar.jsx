57704f531206285a580f3f8639fe8b0deebd0a7c
AabharanGupta <aabharangupta1001@gmail.com>
2023-10-23 21:16:21 +0530

    navbar updated

--- src/components/NNNavbar.jsx
+++ src/components/NNNavbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React,{useState}  from 'react'
 import "./NNNavbar.css";
 import {HashLink as Link} from 'react-router-hash-link';
 
 const NNNavbar = () => {
+    const[MenuOpen,setMenuopen]=useState(false);
   return (
     <nav>
         <Link to="/" className='Title'>ISTE</Link>
-        <div className='Menu'>
+        <div className='Menu' onClick={()=>{
+            setMenuopen(!MenuOpen);
+        }}>
             <span className='line'></span>
             <span className='line'></span>
             <span className='line'></span>
         </div>
-        <ul>
+        <ul className={MenuOpen?"open":""}>
             <li><Link to='#About' smooth>About</Link></li>
             <li><Link to='#RuleBook' smooth>Rulebook</Link></li>
             <li><Link smooth>Our Team</Link></li>
